716cbddc8ea5c33056e5f547bc0431f8f2492bb2
Luis Estrada <lestra@mail.sfsu.edu>
2016-07-13 23:08:26 +0000

    index() renamed back to form()
    -index now just shows all items

--- application/controller/search.php
+++ application/controller/search.php
@@ -6,17 +6,25 @@
 class Search extends Controller
 {    
     /**
-     * This method works with form input to load data to a user
-     * 
-     * Note: It's named 'index' to avoid problems with MINI's unique
-     * way of matching URLs to controller names and their methods
+     * This method prints all items (for testing)
      */
     public function index()
     {
+        $items = $this->item_model->getAllItems();
+        require APP . 'view/_templates/header.php';
+        require APP . 'view/items/index.php';
+        require APP . 'view/_templates/footer.php';
+    }
+    
+    /**
+     * This method works with form input to load data to a user
+     */
+    public function form()
+    {
         if (isset($_GET["submit_search"])) {
 
-            $category = $_GET['category'];
             $input = trim($_GET['searchbar']);
+            $category = $_GET['category'];
             
             // redirect to homepage if user types whitespace only
             if ($input == "") {
@@ -24,12 +32,8 @@ class Search extends Controller
             }
             
             // interact with Item model to get the data, to see if there's a match
-            if ($category == 'All') {
-                $items = $this->item_model->getItemsContaining($input);
-            } else {
-                $items = $this->item_model->getItemsContainingInCategory($input, $category);
-            }
-        
+            $items = $this->item_model->getItemsContaining($input, $category);    
+            
             // load the views depending on whether or not there was a match
             if ($items == null) { // if no results, load notfound page
                 require APP . 'view/_templates/header.php';
@@ -40,9 +44,11 @@ class Search extends Controller
                 require APP . 'view/items/index.php';
                 require APP . 'view/_templates/footer.php';
             }
+            
         } else {
             // redirect to homepage if user visits /search or /search/index
             header('location: ' . URL . 'home/index');
         }
     }
+    
 }
