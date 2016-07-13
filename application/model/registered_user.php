c3d93033a7d42592e7f51e1688121ae9f16f42a6
Luis Estrada <lestra@mail.sfsu.edu>
2016-07-13 23:01:30 +0000

    Add user model

--- application/model/registered_user.php
+++ application/model/registered_user.php
@@ -10,7 +10,7 @@ class RegisteredUser extends Model
 
     /**
      * Update email address
-     @param type $email - the new email address
+     * @param type $email - the new email address
      */
     public function updateEmail($email)
     {
@@ -19,10 +19,56 @@ class RegisteredUser extends Model
     
     /**
      * Update password
-     @param type $pwd - the new password
+     * @param type $pwd - the new password
      */
     public function updatePassword($pwd)
     {
         $password = $pwd;
+    }
+    
+    /**
+     * Add user to the database
+     * @param type $student_id
+     * @param type $username
+     * @param type $email
+     * @param type $password
+     */
+    public function addUser($student_id, $username, $email, $password)
+    {
+        $sql = "INSERT INTO user (student_id, username, email, password) "
+            . "VALUES (:student_id, :username, :email, :password)";
+        $query = $this->db->prepare($sql);
+        $parameters = array(':student_id' => $student_id, ':username' => $username,
+            ':email' => $email, ':password' => $password);
+        $query->execute($parameters);
+    }
+    
+    /**
+     * Add user to the database with profile picture
+     * @param type $student_id
+     * @param type $username
+     * @param type $email
+     * @param type $password
+     * @param type $image - profile picture
+     */
+    public function addUserWithImage($student_id, $username, $email, $password, $image)
+    {
+        $sql = "INSERT INTO user (student_id, username, email, password, image) "
+            . "VALUES (:student_id, :username, :email, :password, :image)";
+        $query = $this->db->prepare($sql);
+        $parameters = array(':student_id' => $student_id, ':username' => $username,
+            ':email' => $email, ':password' => $password, ':image' => $image);
+        $query->execute($parameters);
+    }
+    
+    /**
+     * This function returns all users
+     */
+    public function getAllUsers()
+    {
+        $sql = "SELECT id, student_id, username, email, image FROM user";
+        $query = $this->db->prepare($sql);
+        $query->execute();
+        return $query->fetchAll();
     }
 }
