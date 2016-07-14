a5b9b200aa8a7f26aacb8f0dfc25154fb59d01f8
Luis Estrada <lestra@mail.sfsu.edu>
2016-07-14 21:32:48 +0000

    Add redirections + documentation to Search controller

--- application/controller/search.php
+++ application/controller/search.php
@@ -6,14 +6,12 @@
 class Search extends Controller
 {    
     /**
-     * This method prints all items (for testing)
+     * Redirect to homepage if user visits /search or /search/index since view
+     * doesn't exist! Search controller is only meant to deal with searches.
      */
     public function index()
     {
-        $items = $this->db_model->getAllItems();
-        require APP . 'view/_templates/header.php';
-        require APP . 'view/items/index.php';
-        require APP . 'view/_templates/footer.php';
+        header('location: ' . URL . 'home/index');
     }
     
     /**
@@ -22,11 +20,11 @@ class Search extends Controller
     public function form()
     {
         if (isset($_GET["submit_search"])) {
-
+            
             $input = trim($_GET['searchbar']);
             $category = $_GET['category'];
             
-            // interact with Item model to get the data, to see if there's a match
+            // interact with db model to get the data, to see if there's a match
             $items = $this->db_model->getItemsContaining($input, $category);
             
             // load the views depending on whether or not there was a match
@@ -41,7 +39,8 @@ class Search extends Controller
             }
             
         } else {
-            // redirect to homepage if user visits /search or /search/index
+            // redirect to homepage if user visits /search/form without
+            // actually searching for an item through the search bar
             header('location: ' . URL . 'home/index');
         }
     }
