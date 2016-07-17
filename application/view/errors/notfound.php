5f52b4f3e5cc9fcd4a9a6e988bc8aab30854efd3
Luis Estrada <lestra@mail.sfsu.edu>
2016-07-17 10:41:15 +0000

    Change category even if no search results

--- application/view/errors/notfound.php
+++ application/view/errors/notfound.php
@@ -1,3 +1,13 @@
+<!-- Change category on the search bar if user made a search -->
+<?php
+    if (isset($_GET["submit_search"])) {
+        if ($category != NULL && $category != "All") {
+            echo("<script>$('#category').val('$category');</script>");
+        }
+    }
+?>
+
+<!-- main content -->
 <div class="container">
     Your search '<?php echo($input); ?>' did not match any products.
 </div>
