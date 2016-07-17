0dd2346d1212e768d0edb7a0844b87cbc0b55c46
Luis Estrada <lestra@mail.sfsu.edu>
2016-07-17 12:16:24 +0000

    Fix bugs, typos, style, formatting

--- application/view/items/sell_item.php
+++ application/view/items/sell_item.php
@@ -1,77 +1,78 @@
+<!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <title>ShopU</title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<head>
+    <meta charset="utf-8">
+    <title>ShopU</title>
+    <meta name="description" content="SFSU 648-848 project">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <!-- JS -->
-        <!-- please note: The JavaScript files are loaded in the footer to speed up page construction -->
-        <!-- See more here: http://stackoverflow.com/q/2105327/1114320 -->
+    <!-- JS -->
+    <!-- please note: The JavaScript files are loaded in the footer to speed up page construction -->
+    <!-- See more here: http://stackoverflow.com/q/2105327/1114320 -->
 
-        <!-- CSS -->
-        <link rel="stylesheet" href="<?php echo URL; ?>css/style.css">
+    <!-- MINI CSS -->
+    <link rel="stylesheet" href="<?php echo URL; ?>css/style.css">
 
-        <!-- Bootstrap -->
-        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
 
-        <!-- DataTables -->
-        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.10.12/datatables.min.css"/>
-        <!-- jQuery, loaded in the recommended protocol-less way -->
-        <!-- more http://www.paulirish.com/2010/the-protocol-relative-url/ -->
-        <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+    <!-- jQuery, loaded in the recommended protocol-less way -->
+    <!-- more http://www.paulirish.com/2010/the-protocol-relative-url/ -->
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 
-        <!-- DataTables CSS + JS -->
-        <link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.10.12/css/jquery.dataTables.css">
-        <script type="text/javascript" src="//cdn.datatables.net/1.10.12/js/jquery.dataTables.js"></script>
+    <!-- DataTables CSS + JS -->
+    <link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.10.12/css/jquery.dataTables.css">
+    <script type="text/javascript" src="//cdn.datatables.net/1.10.12/js/jquery.dataTables.js"></script>
 
-    </head>
-    <body>
-        <div class="param">
-            <div class="row">
-                <div class="panel panel-primary">
-                    <div class="panel-body">
-                        <form method="POST" action="#" role="form">
-                            <div class="logo" align ="center">
-                                <a href="<?php echo URL; ?> home/index">
-                                    <img src="<?php echo URL; ?>img/shopu-mod.jpg" alt="ShopU logo"
-                                         style="height:100px"/>
-                                </a>
-                            </div>
+</head>
+<body>
+    
+    <div class="param">
+        <div class="row">
+            <div class="panel panel-primary">
+                <div class="panel-body">
+                    
+                    <!-- form -->
+                    <form action="<?php echo URL; ?>items/additem" method="POST" enctype="multipart/form-data">
+                        <div class="logo" align ="center">
+                            <a href="<?php echo URL; ?>home/index">
+                                <img src="<?php echo URL; ?>img/shopu-mod.jpg" alt="ShopU logo"
+                                     style="height:100px"/>
+                            </a>
+                        </div>
 
-                            <label class="control-label">Title</label>
-                            <input class="form-control" type="text" name="title" required autocomplete="off" value="text"/>
+                        <!-- input fields -->
+                        <label class="control-label">Title</label>
+                        <input class="form-control" type="text" name="title" autocomplete="off" required/>
+                        <label class="control-label">Price</label>
+                        <input class="form-control" type="number" name="price" step="0.01" autocomplete="off" required/>
 
+                        <label>Category</label>
+                        <select class="form-control" name="category">
+                            <option value="Furniture">Furniture</option>
+                            <option value="Electronics">Electronics</option>
+                            <option value="Clothes">Clothes</option>
+                            <option value="Books">Books</option>
+                        </select>
 
-                            <label class="control-label">Price</label>
-                            <input class="form-control" type="number" name="price" required autocomplete="off" value="10.00" step="0.01"/>
-
-
-                            <label>Category</label>
-                            <select class="form-control" name="category">
-                                <option value="Furniture">Furniture</option>
-                                <option value="Electronics">Electronics</option>
-                                <option value="Clothes">Clothes</option>
-                                <option value="Books">Books</option>
-                            </select>
-
-
-                            <div class="shape-group">
-                                <label>Description</label>
-                                <input class="form-control" type="text" name="description" required autocomplete="off" value="text"/>
-                            </div>
-                            <div class="shape-group">
-                                <label>Keywords</label>
-                                <input class="form-control" type="text" name="keywords" required autocomplete="off" value="text"/>
-                            </div>
-                            <div class="shape-group">
-                                <label>Image</label>
-                                <input class="form-control" type="file" name="image"/>
-                            </div><br><br>
-                            <!-- submit button -->
-                            <input class="form-control btn-primary" type="submit" name="submit_add_item" value="Sell Item" />
-                        </form>
-                    </div>
+                        <div class="shape-group">
+                            <label>Description</label>
+                            <input class="form-control" type="text" name="description" autocomplete="off" required/>
+                        </div>
+                        <div class="shape-group">
+                            <label>Keywords</label>
+                            <input class="form-control" type="text" name="keywords" autocomplete="off" required/>
+                        </div>
+                        <div class="shape-group">
+                            <label>Image</label>
+                            <input class="form-control" type="file" name="image"/>
+                        </div><br><br>
+                        
+                        <!-- submit button -->
+                        <input class="form-control btn-primary" type="submit" name="submit_add_item" value="Sell Item" />
+                    </form>
+                    
                 </div>
             </div>
         </div>
+    </div>
--- application/view/users/signup.php
+++ application/view/users/signup.php
@@ -1,80 +1,86 @@
+<!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <title>ShopU</title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<head>
+    <meta charset="utf-8">
+    <title>ShopU</title>
+    <meta name="description" content="SFSU 648-848 project">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <!-- JS -->
-        <!-- please note: The JavaScript files are loaded in the footer to speed up page construction -->
-        <!-- See more here: http://stackoverflow.com/q/2105327/1114320 -->
+    <!-- JS -->
+    <!-- please note: The JavaScript files are loaded in the footer to speed up page construction -->
+    <!-- See more here: http://stackoverflow.com/q/2105327/1114320 -->
 
-        <!-- CSS -->
-        <link rel="stylesheet" href="<?php echo URL; ?>css/style.css">
+    <!-- MINI CSS -->
+    <link rel="stylesheet" href="<?php echo URL; ?>css/style.css">
 
-        <!-- Bootstrap -->
-        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
 
-        <!-- DataTables -->
-        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.10.12/datatables.min.css"/>
-        <!-- jQuery, loaded in the recommended protocol-less way -->
-        <!-- more http://www.paulirish.com/2010/the-protocol-relative-url/ -->
-        <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+    <!-- jQuery, loaded in the recommended protocol-less way -->
+    <!-- more http://www.paulirish.com/2010/the-protocol-relative-url/ -->
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 
-        <!-- DataTables CSS + JS -->
-        <link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.10.12/css/jquery.dataTables.css">
-        <script type="text/javascript" src="//cdn.datatables.net/1.10.12/js/jquery.dataTables.js"></script>
+    <!-- DataTables CSS + JS -->
+    <link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.10.12/css/jquery.dataTables.css">
+    <script type="text/javascript" src="//cdn.datatables.net/1.10.12/js/jquery.dataTables.js"></script>
 
-    </head>
-    <body>
-        <div class="param">
-            <div class="row">
-                <div class="panel panel-primary">
-                    <div class="panel-body">
-                        <form method="POST" action="#" role="form">
-                            <div class="logo" align ="center">
-                                <a href="<?php echo URL; ?> home/index">
-                                    <img src="<?php echo URL; ?>img/shopu-mod.jpg" alt="ShopU logo"
-                                         style="height:100px"/>
-                                </a>
-                            </div>
-                            <div class="shape-group">
-                                <h2>Create account</h2>
-                            </div>
-                            <div class="shape-group">
-                                <label class="control-label">SFSU ID</label>
-                                <input name="student_id" type="text" maxlength="50" class="form-control">
-                            </div>
-                            <div class="shape-group">
-                                <label class="control-label">Username</label>
-                                <input name="username" type="text" autocomplete="off" class="form-control">
-                            </div>
-                            <div class="shape-group">
-                                <label class="control-label">SFSU Email</label>
-                                <input name="email" type="text" autocomplete="off" class="form-control">
-                            </div>
-                            <div class="shape-group">
-                                <label class="control-label">Password</label>
-                                <input name="password" type="password" autocomplete="off" class="form-control" placeholder="at least 6 characters" length="40">
-                            </div>
-                            <div class="shape-group">
-                                <label class="control-label">Confirm Password</label>
-                                <input name="passwordagain" type="password" autocomplete="off" class="form-control" placeholder="at least 6 characters">
-                            </div>
-                            <div class="shape-group">
-                                <label class="control-label">Profile picture</label>
-                                <input name="image" type="file">
-                            </div>
-                            <h2></h2>
-                            <div class="shape-group">
-                                <button name="submit_add_user" type="submit" class="btn btn-info btn-block" value="Sign up">Create your account</button>
-                            </div>
-                            <p class="shape-group">By creating an account, you agree to our <a href="#">Terms of Use</a> and our <a href="#">Privacy Policy</a>.</p>
-                            <hr>
-                            <p></p>Already have an account? <a href="<?php echo URL; ?> users/signin">Sign in</a></p>
-                        </form>
-                    </div>
+</head>
+<body>
+    
+    <div class="param">
+        <div class="row">
+            <div class="panel panel-primary">
+                <div class="panel-body">
+                    
+                    <!-- form -->
+                    <form action="<?php echo URL; ?>users/adduser" method="POST" enctype="multipart/form-data">
+                        <div class="logo" align ="center">
+                            <a href="<?php echo URL; ?>home/index">
+                                <img src="<?php echo URL; ?>img/shopu-mod.jpg" alt="ShopU logo"
+                                     style="height:100px"/>
+                            </a>
+                        </div>
+                        <div class="shape-group text-center">
+                            <h2>Sign Up</h2>
+                        </div>
+                        
+                        <!-- input fields -->
+                        <div class="shape-group">
+                            <label class="control-label">SFSU ID</label>
+                            <input name="student_id" type="text" class="form-control" required>
+                        </div>
+                        <div class="shape-group">
+                            <label class="control-label">Username</label>
+                            <input name="username" type="text" autocomplete="off" class="form-control" required>
+                        </div>
+                        <div class="shape-group">
+                            <label class="control-label">SFSU Email</label>
+                            <input name="email" type="email" autocomplete="off" class="form-control" required>
+                        </div>
+                        <div class="shape-group">
+                            <label class="control-label">Password</label>
+                            <input name="password" type="password" autocomplete="off"
+                                   class="form-control" placeholder="at least 6 characters" required>
+                        </div>
+                        <div class="shape-group">
+                            <label class="control-label">Profile Picture</label>
+                            <input name="image" type="file">
+                        </div><br>
+                        
+                        <!-- sign up -->
+                        <div class="shape-group">
+                            <button name="submit_add_user" type="submit" class="btn btn-info btn-block">
+                                Create your account</button>
+                        </div><br>
+                        <p class="shape-group text-center">By creating an account, you agree to our
+                            <a href="#">Terms of Use</a> and our <a href="#">Privacy Policy</a>.
+                        </p>
+                        <hr>
+                        <p class="text-center">Already have an account?
+                            <a href="<?php echo URL; ?>users/signin">Sign in</a></p>
+                    </form>
                 </div>
             </div>
         </div>
+    </div>
 
