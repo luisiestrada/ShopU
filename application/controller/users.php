01722e18c7fc12710c0a02fab3536356504257f6
Luis Estrada <lestra@mail.sfsu.edu>
2016-07-17 09:21:12 +0000

    Change formatting of controllers to how they were before

--- application/controller/users.php
+++ application/controller/users.php
@@ -3,41 +3,46 @@
 /**
  * Class Users
  */
-class Users extends Controller {
-
+class Users extends Controller
+{
     /**
      * Show all users
      */
-    public function index() {
+    public function index()
+    {
         $users = $this->db_model->getAllUsers();
         require APP . 'view/_templates/header.php';
         require APP . 'view/users/index.php';
         require APP . 'view/_templates/footer.php';
     }
-
-    /**
-     * Go to view/users/signup.php
-     */
-    public function signUp() {
-        require APP . 'view/users/signup.php';
-        require APP . 'view/_templates/footer.php';
-    }
-
+    
     /**
      * Go to view/users/signin.php
      */
-    public function signIn() {
+    public function signIn()
+    {
         require APP . 'view/users/signin.php';
         require APP . 'view/_templates/footer.php';
     }
-
+    
+    /**
+     * Go to view/users/signup.php
+     */
+    public function signUp()
+    {
+        require APP . 'view/users/signup.php';
+        require APP . 'view/_templates/footer.php';
+    }
+    
     /**
      * Add user to database
      */
-    public function addUser() {
+    public function addUser()
+    {
         if (isset($_POST['submit_add_user'])) {
-
+            
             $file = $_FILES["image"]["tmp_name"]; // reference to file uploaded
+            
             // if false, not an image or no image selected
             if ($file != null && getimagesize($file)) {
 
@@ -46,14 +51,18 @@ class Users extends Controller {
                 $image = file_get_contents($file);
 
                 // add user to database with image
-                $this->db_model->addUserWithImage($_POST["student_id"], $_POST["username"], $_POST["email"], $_POST["password"], $image);
+                $this->db_model->addUserWithImage($_POST["student_id"], $_POST["username"],
+                        $_POST["email"], $_POST["password"], $image);
+                
             } else {
                 // add user to database without image (default image used instead)
-                $this->db_model->addUser($_POST["student_id"], $_POST["username"], $_POST["email"], $_POST["password"]);
+                $this->db_model->addUser($_POST["student_id"], $_POST["username"],
+                        $_POST["email"], $_POST["password"]);
             }
-
+            
             // where to go after user is added
             header('location: ' . URL . 'users/index');
+            
         } else {
             // redirect to index if user visits /user/adduser without submitting form
             header('location: ' . URL . 'users/index');
