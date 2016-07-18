2924da69ab363e1f2af34d530bda2b7519281592
Panawat Sanitchon <paneron@mail.sfsu.edu>
2016-07-18 22:55:18 +0000

    remove header from standalone page

--- application/view/items/sell_item.php
+++ application/view/items/sell_item.php
@@ -1,79 +1,50 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <title>ShopU</title>
-    <meta name="description" content="SFSU 648-848 project">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<div class="param">
+    <div class="row">
+        <div class="panel panel-primary">
+            <div class="panel-body">
 
-    <!-- JS -->
-    <!-- please note: The JavaScript files are loaded in the footer to speed up page construction -->
-    <!-- See more here: http://stackoverflow.com/q/2105327/1114320 -->
-
-    <!-- MINI CSS -->
-    <link rel="stylesheet" href="<?php echo URL; ?>css/style.css">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-
-    <!-- jQuery, loaded in the recommended protocol-less way -->
-    <!-- more http://www.paulirish.com/2010/the-protocol-relative-url/ -->
-    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-
-    <!-- DataTables CSS + JS -->
-    <link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.10.12/css/jquery.dataTables.css">
-    <script type="text/javascript" src="//cdn.datatables.net/1.10.12/js/jquery.dataTables.js"></script>
-
-</head>
-<body>
-    
-    <div class="param">
-        <div class="row">
-            <div class="panel panel-primary">
-                <div class="panel-body">
-                    
-                    <div class="logo" align ="center">
-                        <a href="<?php echo URL; ?>home/index">
-                            <img src="<?php echo URL; ?>img/shopu-mod.jpg" alt="ShopU logo"
-                                 style="height:100px"/>
-                        </a>
-                    </div>
-                    
-                    <!-- form -->
-                    <form action="<?php echo URL; ?>items/additem" method="POST" enctype="multipart/form-data">
-                        
-                        <!-- input fields -->
-                        <label class="control-label">Title</label>
-                        <input class="form-control" type="text" name="title" autocomplete="off" required/>
-                        <label class="control-label">Price</label>
-                        <input class="form-control" type="number" name="price" step="0.01" autocomplete="off" required/>
-
-                        <label>Category</label>
-                        <select class="form-control" name="category">
-                            <option value="Furniture">Furniture</option>
-                            <option value="Electronics">Electronics</option>
-                            <option value="Clothes">Clothes</option>
-                            <option value="Books">Books</option>
-                        </select>
-
-                        <div class="shape-group">
-                            <label>Description</label>
-                            <input class="form-control" type="text" name="description" autocomplete="off" required/>
-                        </div>
-                        <div class="shape-group">
-                            <label>Keywords</label>
-                            <input class="form-control" type="text" name="keywords" autocomplete="off" required/>
-                        </div>
-                        <div class="shape-group">
-                            <label>Image</label>
-                            <input type="file" name="image"/>
-                        </div><br><br>
-                        
-                        <!-- submit button -->
-                        <input class="form-control btn-primary" type="submit" name="submit_add_item" value="Sell Item" />
-                    </form>
-                    
+                <div class="logo" align ="center">
+                    <a href="<?php echo URL; ?>home/index">
+                        <img src="<?php echo URL; ?>img/shopu-mod.jpg" alt="ShopU logo"
+                             style="height:100px"/>
+                    </a>
                 </div>
+
+                <!-- form -->
+                <form action="<?php echo URL; ?>items/additem" method="POST" enctype="multipart/form-data">
+
+                    <!-- input fields -->
+                    <label class="control-label">Title</label>
+                    <input class="form-control" type="text" name="title" autocomplete="off" required/>
+                    <label class="control-label">Price</label>
+                    <input class="form-control" type="number" name="price" step="0.01" autocomplete="off" required/>
+
+                    <label>Category</label>
+                    <select class="form-control" name="category">
+                        <option value="Furniture">Furniture</option>
+                        <option value="Electronics">Electronics</option>
+                        <option value="Clothes">Clothes</option>
+                        <option value="Books">Books</option>
+                    </select>
+
+                    <div class="shape-group">
+                        <label>Description</label>
+                        <input class="form-control" type="text" name="description" autocomplete="off" required/>
+                    </div>
+                    <div class="shape-group">
+                        <label>Keywords</label>
+                        <input class="form-control" type="text" name="keywords" autocomplete="off" required/>
+                    </div>
+                    <div class="shape-group">
+                        <label>Image</label>
+                        <input type="file" name="image"/>
+                    </div><br><br>
+
+                    <!-- submit button -->
+                    <input class="form-control btn-primary" type="submit" name="submit_add_item" value="Sell Item" />
+                </form>
+
             </div>
         </div>
     </div>
+</div>
--- application/view/users/signin.php
+++ application/view/users/signin.php
@@ -1,77 +1,48 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <title>ShopU</title>
-    <meta name="description" content="SFSU 648-848 project">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<div class="param">
+    <div class="row">
+        <div class="panel panel-primary">
+            <div class="panel-body">
 
-    <!-- JS -->
-    <!-- please note: The JavaScript files are loaded in the footer to speed up page construction -->
-    <!-- See more here: http://stackoverflow.com/q/2105327/1114320 -->
-
-    <!-- MINI CSS -->
-    <link rel="stylesheet" href="<?php echo URL; ?>css/style.css">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-
-    <!-- jQuery, loaded in the recommended protocol-less way -->
-    <!-- more http://www.paulirish.com/2010/the-protocol-relative-url/ -->
-    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-
-    <!-- DataTables CSS + JS -->
-    <link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.10.12/css/jquery.dataTables.css">
-    <script type="text/javascript" src="//cdn.datatables.net/1.10.12/js/jquery.dataTables.js"></script>
-
-</head>
-<body>
-    
-    <div class="param">
-        <div class="row">
-            <div class="panel panel-primary">
-                <div class="panel-body">
-                    
-                    <div class="logo" align="center">
-                        <a href="<?php echo URL; ?>home/index">
-                            <img src="<?php echo URL; ?>img/shopu-mod.jpg" alt="ShopU logo" />
-                        </a>
-                    </div>
-                    
-                    <!-- form -->
-                    <form action="#" method="POST">
-                        <div class="shape-group text-center">
-                            <h2>Sign in</h2>
-                        </div>
-                        
-                        <!-- input fields -->
-                        <div class="shape-group">
-                            <label>Username</label>
-                            <input name="username" type="text" maxlength="50"
-                                   autocomplete="off" class="form-control" required>
-                        </div>
-                        <div class="shape-group">
-                            <label>Password</label>
-                            <input name="password" type="password" maxlength="25"
-                                   autocomplete="off" class="form-control" required>
-                        </div><br>
-                        
-                        <!-- sign in -->
-                        <div class="shape-group">
-                            <button name="signup" type="submit" class="btn btn-success btn-block">Sign in</button>
-                        </div>
-                        
-                        <!-- create an account -->
-                        <div class="shape-group div">
-                            <hr class="pos-left"><small>New to site?</small><hr class="pos-right">
-                        </div>
-                        <p class="shape-group"><a href="<?php echo URL; ?>users/signup" class="btn btn-info btn-block">Create an account</a></p>
-                        
-                        <!-- terms & conditions -->
-                        <p class="shape-group text-center">By signing in you are agreeing to our 
-                            <a href="#">Terms of Use</a> and our <a href="#">Privacy Policy</a>.</p>
-                    </form>
+                <div class="logo" align="center">
+                    <a href="<?php echo URL; ?>home/index">
+                        <img src="<?php echo URL; ?>img/shopu-mod.jpg" alt="ShopU logo" />
+                    </a>
                 </div>
+
+                <!-- form -->
+                <form action="#" method="POST">
+                    <div class="shape-group text-center">
+                        <h2>Sign in</h2>
+                    </div>
+
+                    <!-- input fields -->
+                    <div class="shape-group">
+                        <label>Username</label>
+                        <input name="username" type="text" maxlength="50"
+                               autocomplete="off" class="form-control" required>
+                    </div>
+                    <div class="shape-group">
+                        <label>Password</label>
+                        <input name="password" type="password" maxlength="25"
+                               autocomplete="off" class="form-control" required>
+                    </div><br>
+
+                    <!-- sign in -->
+                    <div class="shape-group">
+                        <button name="signup" type="submit" class="btn btn-success btn-block">Sign in</button>
+                    </div>
+
+                    <!-- create an account -->
+                    <div class="shape-group div">
+                        <hr class="pos-left"><small>New to site?</small><hr class="pos-right">
+                    </div>
+                    <p class="shape-group"><a href="<?php echo URL; ?>users/signup" class="btn btn-info btn-block">Create an account</a></p>
+
+                    <!-- terms & conditions -->
+                    <p class="shape-group text-center">By signing in you are agreeing to our 
+                        <a href="#">Terms of Use</a> and our <a href="#">Privacy Policy</a>.</p>
+                </form>
             </div>
         </div>
     </div>
+</div>
