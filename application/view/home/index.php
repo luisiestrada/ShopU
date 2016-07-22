5f5f8ddf852aa30415e266841b7e668b60f5156e
Luis Estrada <lestra@mail.sfsu.edu>
2016-07-22 03:34:58 +0000

    Add homepage msg to users signed in

--- application/view/home/index.php
+++ application/view/home/index.php
@@ -1,7 +1,16 @@
 <div class="container-fluid bg text-center">
     
     <div id="homepage-text">
-        <h1>Welcome to ShopU!</h1>
+        <?php
+            if (isset($_SESSION['username']) === false) {
+                echo("<h1>Welcome to ShopU!</h1>");
+                echo "Session Id: " . session_id() . "<br>";
+            } else {
+                echo("<h1>Welcome, " . $_SESSION['username'] . "!</h1>");
+                echo "Session Id: " . session_id() . "<br>";
+            }
+        ?>
+        
         <h2>
             ShopU is an electronic commerce website that provides
             consumer-to-consumer sales services exclusively to students
