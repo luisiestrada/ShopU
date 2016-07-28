3c20480b1a801ac3a33dd62423581bfcb533fc78
qkuang <qkuang@mail.sfsuswe.edu>
2016-07-28 21:21:14 +0000

    remove redundant comments

--- application/view/items/itemConfirmation.php
+++ application/view/items/itemConfirmation.php
@@ -127,14 +127,6 @@
 </div>
 <!--Order button End-->
 
-
-
-
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
